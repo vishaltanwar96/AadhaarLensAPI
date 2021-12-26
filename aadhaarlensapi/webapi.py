a83d6703bbce36bd13067f3ec6d642416e94ec95
Vishal Tanwar <vishal.tanwar@yougov.com>
2021-12-26 17:57:51 +0530

    allow cors

--- aadhaarlensapi/webapi.py
+++ aadhaarlensapi/webapi.py
@@ -3,8 +3,10 @@ from aadhaar.secure_qr.exceptions import MalformedDataReceived
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/api/extract/", methods=["POST"])
